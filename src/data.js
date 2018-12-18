3d3351da9f2a861277709bdd5ef2bfaae5a70240
GloryRojas <grojasm94@gmail.com>
2018-12-18 15:58:14 -0500

    Aplicando evento click al DOM para ver diferentes pantallas

--- src/data.js
+++ src/data.js
@@ -1,32 +1,34 @@
-//Construyendo función para copiar la data
+// Construyendo función para copiar la data
+
 const arrpokemon = POKEMON.pokemon;
 const datacopy = (arr) => {
- const cloneArrPokemon = [];
- for(let i = 0; i < arr.length; i++){
-   cloneArrPokemon.push(Object.assign({}, arr[i]));
- }
- return cloneArrPokemon;
+  const cloneArrPokemon = [];
+  for (let i = 0; i < arr.length; i++) {
+    cloneArrPokemon.push(Object.assign({}, arr[i]));
+  }
+  return cloneArrPokemon;
 };
-//Nuevo array : console.log(datacopy(arrpokemon));
-//Array original : console.log(arrpokemon);
+// Nuevo array : console.log(datacopy(arrpokemon));
+// Array original : console.log(arrpokemon);
 
-//Array copia de la data original ---> Aqui iniciamos la funcionalidad.
+// Array copia de la data original ---> Aqui iniciamos la funcionalidad.
+
 const dataPokemon = datacopy(arrpokemon);
 
-//Funcion para pintar tarjetas con datos pokemon
+// Funcion para pintar tarjetas con datos pokemon
 const crearTemplateDeCard = (data) => {
   let templateListOfCards = '';
   // recorremos nuestro array con forEach
   data.forEach((pokemon) => {
     // creamos un template(string) por cada elemento del array
     const card = `
-      <div class="card-link">
-        <article class="blog-card">
-          <img class="post-image" src="${ pokemon.img }" />
-          <h3 class="post-title">${ pokemon.name }</h3>
-          <div class="article-details">
-            <h4 class="post-category">${ pokemon.type }</h4>
-            <p class="post-description">Altura: ${ pokemon.height }</p>
+      <div class='card-link'>
+        <article class='blog-card'>
+          <img class='post-image' src='${ pokemon.img }' />
+          <h3 class='post-title'>${ pokemon.name }</h3>
+          <div class='article-details'>
+            <h4 class='post-category'>${ pokemon.type }</h4>
+            <p class='post-description'>Altura: ${ pokemon.height }</p>
           </div>
         </article>
       </div>
@@ -37,19 +39,20 @@ const crearTemplateDeCard = (data) => {
 };
 
 
-//Obteniendo objetos con pokemons mas evolucionados
-
+// Obteniendo objetos con pokemons mas evolucionados
 const mostEvolutionPokemon = dataPokemon.filter(evolucion => (evolucion.candy_count === undefined));
 
-//Filtrando pokemones según tipo
+// Filtrando pokemones según condition  'Tipo'
 const filterData = (data, condition)=>{
   let arrType = [];
-  for (var i = 0; i < data.length; i++) {
-    for (var j = 0; j < data[i].type.length; j++) {
-      if(data [i].type[j] == condition){
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].type.length; j++) {
+      if (data [i].type[j] === condition) {
         arrType.push(data[i])
       }
     }
   }
   return arrType;
 };
+
+// Funcion ORDENAR
--- src/main.js
+++ src/main.js
@@ -1,16 +1,51 @@
-//const ulContainer = document.getElementById('container-list');
-//ulContainer.innerHTML = templateList;
-//Mostrando todos los pokemones.
-const containerList = document.getElementById("container-list");
+//  const ulContainer = document.getElementById('container-list');
+//  ulContainer.innerHTML = templateList;
+//  Mostrando todos los pokemones.
+const containerList = document.getElementById('container-list');
 containerList.innerHTML = crearTemplateDeCard(dataPokemon);
 
-//Mostrando los mas evolucionados
-const mostEvolution = document.getElementById("most-evolution");
-mostEvolution.addEventListener("click", () =>{
+//  Mostrando los mas evolucionados
+const mostEvolution = document.getElementById('most-evolution');
+mostEvolution.addEventListener('click', () => {
   containerList.innerHTML = crearTemplateDeCard(mostEvolutionPokemon);
 });
+//  Mostrando pokemones segun tipo:
+const containerTypes = document.getElementById('cont-tipos');
+containerTypes.addEventListener('click', (e) => {
+  containerList.innerHTML = crearTemplateDeCard(filterData(dataPokemon, e.target.getAttribute('value')))
+});
 
-const containerTypes = document.getElementById("cont-tipos");
-containerTypes.addEventListener("click", (e) =>{
-  containerList.innerHTML = crearTemplateDeCard(filterData(dataPokemon, e.target.getAttribute("value")))
+const pagInicio = document.getElementById('pag-inicio');
+const pagFiltrar = document.getElementById('pag-filtrar');
+const pagOrdenar = document.getElementById('pag-ordenar');
+const pagPokecientifico = document.getElementById('pag-pokecientifico');
+
+const butonInicio = document.getElementById('btn-inicio');
+const butonFiltrar = document.getElementById('btn-filtrar');
+const butonOrdenar = document.getElementById('btn-ordenar');
+const butonPokecientifico = document.getElementById('btn-pokecientifico');
+
+butonInicio.addEventListener("click", () => {
+  pagInicio.style.display = "block";
+  pagFiltrar.style.display = "none";
+  pagOrdenar.style.display = "none";
+  pagPokecientifico.style.display = "none";
+});
+butonFiltrar.addEventListener("click", () => {
+  pagInicio.style.display = "none";
+  pagFiltrar.style.display = "block";
+  pagOrdenar.style.display = "none";
+  pagPokecientifico.style.display = "none";
+});
+butonOrdenar.addEventListener("click", () => {
+  pagInicio.style.display = "none";
+  pagFiltrar.style.display = "none";
+  pagOrdenar.style.display = "block";
+  pagPokecientifico.style.display = "none";
+});
+butonPokecientifico.addEventListener("click", () => {
+  pagInicio.style.display = "none";
+  pagFiltrar.style.display = "none";
+  pagOrdenar.style.display = "none";
+  pagPokecientifico.style.display = "block";
 });
