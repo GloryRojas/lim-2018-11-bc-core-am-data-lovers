b79d1fec68af616e498155a036229a016f9b7b0c
mayrat <mayrat.casavilca@gmail.com>
2018-12-20 17:56:33 -0500

    creando funcion sort para ordenar alfabeticamente los nombres de pokemones

--- src/data.js
+++ src/data.js
@@ -1,8 +1,66 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
+/*
 const example = () => {
   return 'example';
 };
 
 window.example = example;
+containerList.innerHTML= templateCard (arrPokemon);
+*/
+const arrPokemon = POKEMON.pokemon;
+// funcion para rellenar tarjetas con datos de pokemones
+const templateCard = (data) => {
+  let templateListOfCards = '';
+  // recorremos  nuestra array con forEach
+  data.forEach((pokemon) => {
+    const card = `
+      <div class="card-link">
+        <article class="blog-card">
+          <img class="post-image" src="${pokemon.img}"/>
+          <h3 class="post-tittle">${pokemon.name}</h3>
+          <div class="article-details">
+            <h4 class="post-category">${pokemon.type}</h4>
+            <p class="post-description">Altura: ${pokemon.height}</p>
+          </div>
+        </article>
+      </div>  
+    ` ;
+    templateListOfCards += card;
+  })
+  return templateListOfCards;
+}
+
+const showEvolution = arrPokemon.filter(evolution => (evolution.candy-count === undefined));
+
+// filtro de pokemones segun tipo:
+const filterData = (data, condition) => {
+  let arrType = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let x = 0; x < data[i].type.length; x++) {
+      if (data[i].type[x] === condition) {
+        arrType.push(data[i]);
+      }
+    }
+  }
+  return arrType;
+}
+// Funcion ordenar
+const sortData = (data, sortBy, sortOrder) => {
+  let arrAscenVar;
+  if (sortBy === 'name') {
+    arrAscenVar = data.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name === b.name) {
+        return 0;
+      }
+      return -1;
+    });
+  }
+  if (sortOrder === 'descendente') {
+    arrAscenVar = arrAscenVar.reverse();
+  }
+  return arrAscenVar;
+};
--- src/main.js
+++ src/main.js
@@ -1,27 +1,136 @@
+const containerList = document.getElementById('contPokemones');
+
+//funcion monstrar tipos de pokemones
+const containerType = document.getElementById('contType');
+containerType.addEventListener('click', (event) => {
+  containerList.innerHTML = templateCard(filterData(arrPokemon, event.target.getAttribute("value")));
+});
+
+const pageInit = document.getElementById('page-init');
+const pageOrder = document.getElementById('page-order');
+const pageFilter = document.getElementById('page-filter');
+const pageFinal = document.getElementById('page-final');
+
+const btnInit = document.getElementById('btn-init');
+const btnFilter = document.getElementById('btn-filter');
+const btnOrder = document.getElementById('btn-order');
+const btnFinal = document.getElementById('btn-final');
+
+btnInit.addEventListener('click', () => {
+  pageInit.style.display = 'block';
+  pageFilter.style.display = 'none';
+  pageOrder.style.display = 'none';
+  pageFinal.style.display = 'none';
+});
+
+btnFilter.addEventListener('click', () => {
+  pageInit.style.display = 'none';
+  pageFilter.style.display = 'block';
+  pageOrder.style.display = 'none';
+  pageFinal.style.display = 'none';
+});
+
+btnOrder.addEventListener('click', () => {
+  pageInit.style.display = 'none';
+  pageFilter.style.display = 'none';
+  pageOrder.style.display = 'block';
+  pageFinal.style.display = 'none';
+});
+
+btnFinal.addEventListener('click', () => {
+  pageInit.style.display = 'none';
+  pageFilter.style.display = 'none';
+  pageOrder.style.display = 'none';
+  pageFinal.style.display = 'block';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
 
 const htmlOriginal = document.getElementById("contPokemones").innerHTML;
+
 var arrPokemon = POKEMON.pokemon;
-
 document.getElementById("sec3Tit1").innerHTML = arrPokemon[0].name;
-document.getElementById("sec3Tit2").innerHTML = arrPokemon[1].name;
-document.getElementById("sec3Tit3").innerHTML = arrPokemon[2].name;
-
-
+document.getElementById("sec3Tit2").innerHTML = arrPokemon[104].name;
+document.getElementById("sec3Tit3").innerHTML = arrPokemon[15].name;
 
 document.getElementById("sec3Img1").src = arrPokemon[0].img;
 //solo se ejecuta una vez
-let pWater = [];
-for (let i = 0; i < arrPokemon.length; i++) {
-  for (let x = 0; x < arrPokemon[i].type.length; x++) {
-    if (arrPokemon[i].type[x] === "Water") {
-      pWater.push(arrPokemon[i])
-    }
-  }
-}
 
 function btnPokemonWater() {
   let newHtmlGeneration = "";
-  document.getElementById("contTypePok").innerHTML="Tipo Agua";
+  document.getElementById("contTypePok").innerHTML = "Tipo Agua";
   for (let i = 0; i < pWater.length; i++) {
     newHtmlGeneration += htmlOriginal.replace("*|img|*", pWater[i].img).replace("*|name|*", pWater[i].name);
   }
@@ -39,58 +148,47 @@ for (let i = 0; i < arrPokemon.length; i++) {
   }
 }
 
-function btnPokemonGround(){
-  let newHtmlGeneration="";
-  document.getElementById("contTypePok").innerHTML="Tipo Tierra";
+function btnPokemonGround() {
+  let newHtmlGeneration = "";
+  document.getElementById("contTypePok").innerHTML = "Tipo Tierra";
   for (let i = 0; i < pGround.length; i++) {
-    newHtmlGeneration += htmlOriginal.replace("*|img|*",pGround[i].img).replace("*|name|*", pGround[i].name);
+    newHtmlGeneration += htmlOriginal.replace("*|img|*", pGround[i].img).replace("*|name|*", pGround[i].name);
   }
   document.getElementById("contPokemones").innerHTML = newHtmlGeneration;
 }
 
 document.getElementById("sec3Img3").src = arrPokemon[15].img;
 //solo se ejecuta una vez
-let pFlying=[];
+let pFlying = [];
 for (let i = 0; i < arrPokemon.length; i++) {
   for (let x = 0; x < arrPokemon[i].type.length; x++) {
-    if (arrPokemon[i].type[x]==="Flying") {
+    if (arrPokemon[i].type[x] === "Flying") {
       pFlying.push(arrPokemon[i])
     }
   }
 }
 
-function btnPokemonFlying(){
-  let newHtmlGeneration="";
-  document.getElementById("contTypePok").innerHTML="Tipo Aire";
+function btnPokemonFlying() {
+  let newHtmlGeneration = "";
+  document.getElementById("contTypePok").innerHTML = "Tipo Aire";
   for (let i = 0; i < pFlying.length; i++) {
-    newHtmlGeneration += htmlOriginal.replace("*|img|*",pFlying[i].img).replace("*|name|*", pFlying[i].name);
+    newHtmlGeneration += htmlOriginal.replace("*|img|*", pFlying[i].img).replace("*|name|*", pFlying[i].name);
   }
   document.getElementById("contPokemones").innerHTML = newHtmlGeneration;
 }
 
+for (let i = 0; i < POKEMON.pokemon.length; i++) {
+  console.log(POKEMON.pokemon[i].type);
+  //document.getElementById("contPokem").innerHTML += "<p>" + POKEMON.pokemon[i].name+ " "+ POKEMON.pokemon[i].num + "</p>";
+}
 
 
-
-
-
-
-
-
-
-
-
-/*for (let i = 0; i < POKEMON.pokemon.length; i++) {
-    //console.log(POKEMON.pokemon[i].name + " " +POKEMON.pokemon[i].egg+" "+ POKEMON.pokemon[i].num);
-     document.getElementById("contPokem").innerHTML += "<p>" + POKEMON.pokemon[i].name+ " "+ POKEMON.pokemon[i].num + "</p>";
-    }
-  */
- /*
 var arrEvolution = [];
 var arrUndef = [];
 for (let i = 0; i < arrPokemon.length; i++) {
-  arrEvolution.push(arrPokemon[i].candy_count);
-  if (arrPokemon[i].candy_count === undefined) {
-    arrUndef.push(arrPokemon[i].name)
-  }
+ arrEvolution.push(arrPokemon[i].candy_count);
+ if (arrPokemon[i].candy_count === undefined) {
+   arrUndef.push(arrPokemon[i].name)
+ }
 }
 */
